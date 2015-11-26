dabfc2b214bac75488e51afd7fe4db59ee6b0983
Grant Richmond <accounts@terminalpixel.co.uk>
2015-11-26 12:25:01 +0000

    big update

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ nconf.argv()
     .env()
     .file({ file: './config.json' });
 
-var pushBulletTokens = nconf.get('pushbulletTokens');
+var users = nconf.get('users');
+var baseUrl = nconf.get('baseUrl');
 
 var OAuth2 = OAuth.OAuth2;
 var clientId = nconf.get('pushbulletClientId');
@@ -24,25 +25,56 @@ var oauth2 = new OAuth2(
     null
 );
 
+var getUserByToken = function(token, cb) {
+  var pusher = new PushBullet(token);
+  pusher.me(function(err, user) {
+    if (err) {
+      cb(false);
+    } else {
+      user.token = token;
+      if (users.indexOf(user) > -1) {
+        // We found the user
+        cb(user);
+      } else {
+        cb(false);
+      }
+    }
+  });
+};
+
+var getUserByIden = function(iden, cb) {
+  var foundUsers = users.filter(function(user) {
+    return (user.iden == iden);
+  });
+  if (foundUsers.length === 1) {
+    cb(foundUsers[0]);
+  } else {
+    cb(false);
+  }
+};
 
 var notifyTheFleeple = function(message) {
-    for (var i = pushBulletTokens.length - 1; i >= 0; i--) {
-        var key = pushBulletTokens[i];
-        var pusher = new PushBullet(key);
-        console.log('push?');
-        pusher.note({}, 'For the Fleeple', message, function(err, res) {
-            console.log(res);
-        });
+    for (var i = users.length - 1; i >= 0; i--) {
+        var user = users[i];
+        var pusher = new PushBullet(user.token);
+        pusher.note({}, 'For the Fleeple', message);
+    }
+};
+
+var pollTheFleeple = function(message) {
+    for (var i = users.length - 1; i >= 0; i--) {
+        var user = users[i];
+        var pusher = new PushBullet(user.token);
+        pusher.link({}, 'For the Fleeple: ' + message, baseUrl + '/poll?iden=' + user.iden);
     }
 };
 
 spark.on('login', function() {
     console.log('logged in and running');
-    notifyTheFleeple('The server has restarted');
 
     spark.onEvent('fleet-bacon', function(data) {
       console.log("Bacon Event: " + data);
-      notifyTheFleeple('Someone is going for bacon! Click if you want some or something?');
+      pollTheFleeple('Someone is going for bacon! Click the link if you want some.');
     });
 
     spark.onEvent('fleet-coffee-on', function(data) {
@@ -57,18 +89,18 @@ spark.on('login', function() {
 
     spark.onEvent('fleet-beer', function(data) {
       console.log("It's beer o clock: " + data);
-      notifyTheFleeple('What time is it? It\'s beer\'o\'clock!');
+      pollTheFleeple('What time is it? It\'s beer\'o\'clock! Click if you\'re coming along');
     });
 });
 
 app.get('/', function (req, res) {
   var authURL = oauth2.getAuthorizeUrl({
-      redirect_uri: 'http://localhost:8080/code',
+      redirect_uri: baseUrl + '/code',
       state: 'this is just some gobbledygook for security',
       response_type: 'code'
   });
 
-  var body = '<a href="' + authURL + '"> Get Code </a>';
+  var body = '<a href="' + authURL + '">Login with PushBullet</a>';
   res.end(body);
 });
 
@@ -77,8 +109,6 @@ app.get('/code', function (req, res) {
         oauth2.getOAuthAccessToken(
             req.query.code,
             {
-                // 'redirect_uri': 'http://localhost:8080/code/',
-
                 'grant_type': 'authorization_code'
             },
             function (e, access_token, refresh_token, results){
@@ -91,13 +121,27 @@ app.get('/code', function (req, res) {
                 }
                 else {
                     console.log('Obtained access_token: ', access_token);
-                    if (!pushBulletTokens) {
-                      pushBulletTokens = [];
+                    if (!users) {
+                      users = [];
                     }
-                    pushBulletTokens.push(access_token);
-                    nconf.set('pushbulletTokens', pushBulletTokens);
-                    nconf.save();
-                    res.end('Awesome! You will be notified of stuff');
+                    var pusher = new PushBullet(access_token);
+                    pusher.me(function(err, user) {
+                      if (err) {
+                        res.end('Couldn\'t get your profile :(');
+                      } else {
+                        getUserByIden(user.iden, function(foundUser) {
+                          if (!foundUser) {
+                            user.token = access_token;
+                            users.push(user);
+                            nconf.set('users', users);
+                            nconf.save();
+                          } else {
+                            user = foundUser;
+                          }
+                        });
+                        res.end('<p>Awesome! You will be notified of stuff</p><p><a href="/bacon?iden=' + user.iden + '">Click here if you are getting bacon</a></p><p><a href="/unsubscribe?iden=' + user.iden + '"">Click here to unsubscribe</a></p>');
+                      }
+                    });
                 }
         });
     } else {
@@ -105,6 +149,50 @@ app.get('/code', function (req, res) {
     }
 });
 
-app.listen(8080);
+app.get('/poll', function (req, res) {
+  if (req.query.iden) {
+    getUserByIden(req.query.iden, function(user){
+      if (user) {
+        notifyTheFleeple(user.name + ' is in!');
+        res.end('You\'re in!');
+      } else {
+        res.end('Uh oh!');
+      }
+    });
+  }
+});
 
-spark.login({username: 'richmond.grant@gmail.com', password: 'JTNWrc4lnhGa7fh9Idd4IwhAh7kRo2uu'});
+app.get('/bacon', function (req, res) {
+  if (req.query.iden) {
+    getUserByIden(req.query.iden, function(user){
+      if (user) {
+        pollTheFleeple(user.name + ' is getting bacon. Click the link if you want some.');
+        res.end('The Fleeple have been questioned');
+      } else {
+        res.end('Uh oh!');
+      }
+    });
+  }
+});
+
+app.get('/unsubscribe', function (req, res) {
+  if (req.query.iden) {
+    getUserByIden(req.query.iden, function(user){
+      if (user) {
+        // We have found this user in the keys
+        var i = users.indexOf(user);
+        users.splice(i, 1);
+        nconf.set('users', users);
+        nconf.save();
+      }
+    });
+  }
+  res.end('You will no longer be notified');
+});
+
+spark.login({
+  username: nconf.get('particleUsername'),
+  password: nconf.get('particlePassword')
+});
+
+app.listen(nconf.get('port'));
